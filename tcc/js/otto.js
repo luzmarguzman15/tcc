5f8cf0f639c40a7765cafa0ba3a1f5140ee8e022
luzmarguzman15 <luzmar.ramos@escola.pr.gov.br>
2026-08-20 23:28:22 -0300

    dwdcxa
    cderece

--- tcc/js/otto.js
+++ tcc/js/otto.js
@@ -1,10 +1,9 @@
-// Lógica inicial da página
+
 document.addEventListener("DOMContentLoaded", () => {
     
-    // Seleciona todos os botões de comprar
+    
     const botoesComprar = document.querySelectorAll(".btn-buy");
 
-    // Adiciona evento de clique para cada botão
     botoesComprar.forEach((botao, index) => {
         botao.addEventListener("click", () => {
             alert(`Produto ${index + 1} adicionado ao carrinho!`);
